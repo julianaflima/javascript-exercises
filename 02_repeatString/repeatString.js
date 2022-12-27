41144bc71324c606890791dcd0c5fff1a4b86bf4
julianaflima <juli.faccio@gmail.com>
2022-12-27 08:33:55 +0530

    Solution to ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+let newString;
 
+const repeatString = function(string, times) {
+	newString = '';
+	if (times < 0) return `ERROR`;
+	for (let i = 0; i < times; i++) {
+		newString += string;
+	}
+	return newString;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
