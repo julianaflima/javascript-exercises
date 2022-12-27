b545717c0d265af4722f9cc7a947b8dc61a2c651
julianaflima <juli.faccio@gmail.com>
2022-12-27 12:08:52 +0530

    Solution to ex 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,28 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	// Get last letter
+	let letterIndex = string.length - 1;
+	//console.log(letterIndex);
+	let reverseOrder = '';
+	for(i = letterIndex; i >= 0; i--) {
+		let currentLetter = string.substr(i,1);
+		//console.log(currentLetter);	
+		reverseOrder += currentLetter;		
+	}
+	//console.log(reverseOrder);
+	return reverseOrder
 };
+
+
+//console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+// There's another way of doing this that turns strings into arrays
+
+//const reverseArray = function(string) {
+//	console.log(string.split('').reverse().join(''));
+//}
+
+//reverseArray('hello')
