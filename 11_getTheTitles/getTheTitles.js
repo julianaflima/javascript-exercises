5886b0357730e386de994b3757aad78918e21f08
julianaflima <juli.faccio@gmail.com>
2023-01-07 12:55:48 +0530

    Solution to ex 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+	const result = array.map(book => book["title"]);
+	return result;	
 };
+
+// const books = [
+//       {
+//         title: 'Book',
+//         author: 'Name'
+//       },
+//       {
+//         title: 'Book2',
+//         author: 'Name2'
+//       }
+//     ]
+
+// console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
