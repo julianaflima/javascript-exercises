fc3cc9aebef5a6d544533031a861f21272ef5625
julianaflima <juli.faccio@gmail.com>
2023-01-07 09:42:22 +0530

    Solution to ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
 
+	let result = +position < 0 ? "OOPS" :
+	+position === 0 ? 0 :
+	+position === 1 ? 1 :
+	fibonacci(+position - 1) + fibonacci(+position - 2);
+
+	return result;
 };
+
+// console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
