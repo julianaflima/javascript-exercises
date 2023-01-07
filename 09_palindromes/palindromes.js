c5c62f5581fb4a1d3dc4f944db3baa2adf596e1a
julianaflima <juli.faccio@gmail.com>
2023-01-07 08:04:21 +0530

    Solution to ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,42 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	let stringForwads = '';
+	let stringBackwards = '';
+
+	for (let i = string.length - 1; i >= 0; i--) {
+		stringBackwards = (string.charAt(i) !== '' && 
+			string.charAt(i).toLowerCase() !== string.charAt(i).toUpperCase()) ? 
+			stringBackwards += string.charAt(i).toLowerCase() : stringBackwards;
+
+		// stringForwards = removeNonLetters(string.charAt(i));
+	}
+
+	for (let i = 0; i <= string.length - 1; i++) {
+		stringForwads = (string.charAt(i) !== '' && 
+			string.charAt(i).toLowerCase() !== string.charAt(i).toUpperCase()) ?
+			stringForwads += string.charAt(i).toLowerCase() :
+			stringForwads;	
+	}
+
+	// console.log(stringForwads);
+	// console.log(stringBackwards);
+	
+	let result = (stringForwads === stringBackwards) ? true : false;
+	return result;
+
 
 };
+
+// function removeNonLetters(character) {
+// 	if (character !== '' && character.toLowerCase() !== character.toUpperCase) {
+// 		return character;
+// 	}
+// }
+
+// console.log(removeNonLetters(''))
+
+// palindromes('Ab');
+
+// console.log(palindromes('Rats live on no evil star.'));
 
 // Do not edit below this line
 module.exports = palindromes;
